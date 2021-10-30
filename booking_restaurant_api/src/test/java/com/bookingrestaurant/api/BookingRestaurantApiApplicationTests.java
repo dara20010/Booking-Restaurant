fac3c99be5cdbb3d40e6d18dcd07c4673c853508
Maria Dariana <77814319+dara20010@users.noreply.github.com>
2021-10-29 21:54:50 -0500

    Inicio del proyecto

---
+++ booking_restaurant_api/src/test/java/com/bookingrestaurant/api/BookingRestaurantApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bookingrestaurant.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookingRestaurantApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
